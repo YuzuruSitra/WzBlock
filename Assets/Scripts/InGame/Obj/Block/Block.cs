dc3a2251fd696e8bb4b0b146b5b7d798426d6336
Sitra <yuzurusitra@gmail.com>
2024-07-27 11:53:37 +0900

    ブロッククラスのリファクタリング

--- Assets/Scripts/InGame/Obj/Block/Block.cs
+++ Assets/Scripts/InGame/Obj/Block/Block.cs
@@ -7,42 +7,38 @@ namespace InGame.Obj.Block
 {
     public class Block : MonoBehaviour
     {
-        private GameStateHandler _gameStateHandler;
-        public event Action<Block> OnReturnToPool;
-        
         [SerializeField] private int _score;
-        private ScoreHandler _scoreHandler;
         [SerializeField] private GameObject _hitEffect;
+        [SerializeField] private BoxCollider _col;
+        [SerializeField] private MeshRenderer _mesh;
+        [SerializeField] private Rigidbody _rb;
+        [SerializeField] private float _speed;
+        
+        private GameStateHandler _gameStateHandler;
+        private ScoreHandler _scoreHandler;
         private ParticleSystem _ps;
         private WaitForSeconds _wait;
-        [SerializeField] 
-        private BoxCollider _col;
-        [SerializeField] 
-        private MeshRenderer _mesh;
-        [SerializeField]
-        private Rigidbody _rb;
-        [SerializeField]
-        private float _speed;
         private float _yLimit;
         private Coroutine _coroutine;
         private ComboCounter _comboCounter;
         private bool _isActive;
+        public event Action<Block> OnReturnToPool;
 
-        private void Start()
+        private void Awake()
         {
             _scoreHandler = ScoreHandler.Instance;
             _ps = _hitEffect.GetComponent<ParticleSystem>();
             _gameStateHandler = GameStateHandler.Instance;
-            _gameStateHandler.ChangeGameState += ChangeState;
+            _comboCounter = ComboCounter.Instance;
             _wait = new WaitForSeconds(_ps.main.duration);
             _yLimit = GameObject.FindWithTag("CubeLim").transform.position.y + transform.localScale.y / 2.0f;
-            _comboCounter = ComboCounter.Instance;
+            _gameStateHandler.ChangeGameState += ChangeState;
         }
 
         private void Update()
         {
-            if (!_isActive) return;
-            if (_yLimit > transform.position.y) ReturnBlock();
+            if (!_isActive || !(transform.position.y < _yLimit)) return;
+            ReturnBlock();
         }
 
         private void FixedUpdate()
@@ -59,22 +55,17 @@ namespace InGame.Obj.Block
                     _rb.velocity = Vector3.zero;
                     break;
                 case GameStateHandler.GameState.Launch:
-                    if (_isActive) ReturnBlock();
+                    if (_isActive)
+                    {
+                        ReturnBlock();
+                    }
                     break;
             }
         }
 
         public void Activate()
         {
-            ChangeLookActive(true);
-        }
-
-        private void ChangeLookActive(bool newActive)
-        {
-            _isActive = newActive;
-            _col.enabled = newActive;
-            _mesh.enabled = newActive;
-            if (newActive) ChangeParticleSystem(false);
+            SetActiveState(true);
         }
 
         private void HitBall()
@@ -86,47 +77,52 @@ namespace InGame.Obj.Block
 
         private IEnumerator BreakWallAnim()
         {
-            ChangeLookActive(false);
-            ChangeParticleSystem(true);
+            SetActiveState(false);
+            ToggleParticleSystem(true);
             yield return _wait;
-            ChangeParticleSystem(false);
+            ToggleParticleSystem(false);
             _coroutine = null;
             OnReturnToPool?.Invoke(this);
         }
 
         private void OnCollisionEnter(Collision collision)
         {
-            if (collision.gameObject.CompareTag("Ball"))
-                HitBall();
+            if (collision.gameObject.CompareTag("Ball")) HitBall();
         }
 
         private void ReturnBlock()
         {
-            if (_coroutine != null) 
+            if (_coroutine != null)
             {
                 StopCoroutine(_coroutine);
                 _coroutine = null;
-                ChangeParticleSystem(false);
             }
-            ChangeLookActive(false);
-            OnReturnToPool?.Invoke(this);    
+            SetActiveState(false);
+            OnReturnToPool?.Invoke(this);
         }
 
-        private void ChangeParticleSystem(bool isPlay)
+        private void SetActiveState(bool isActive)
+        {
+            _isActive = isActive;
+            _col.enabled = isActive;
+            _mesh.enabled = isActive;
+            if (isActive) ToggleParticleSystem(false);
+        }
+
+        private void ToggleParticleSystem(bool isPlay)
         {
             if (!_ps) return;
-            if (isPlay)
+
+            switch (isPlay)
             {
-                if (_ps.isPlaying) return;
-                _ps.Play();
-            }
-            else
-            {
-                if (!_ps.isPlaying) return;
-                _ps.Stop();
-                _ps.Clear();
+                case true when !_ps.isPlaying:
+                    _ps.Play();
+                    break;
+                case false when _ps.isPlaying:
+                    _ps.Stop();
+                    _ps.Clear();
+                    break;
             }
         }
-
     }
 }
